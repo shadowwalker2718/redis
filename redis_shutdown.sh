bf8fcf79767f0f66ca5f32ef18591ef60ca336de
Shadow Walker <shadowwalker2718@gmail.com>
2017-07-23 22:01:39 -0400

    redis start/stop

--- redis_shutdown.sh
+++ redis_shutdown.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-ssh u0 /opt/share/redis/src/redis-cli -h 192.168.0.100 -p 6379 shutdown
-ssh u1 /opt/share/redis/src/redis-cli -h 192.168.0.101 -p 6380 shutdown
-ssh u2 /opt/share/redis/src/redis-cli -h 192.168.0.102 -p 6381 shutdown
+##ssh u0 /opt/share/redis/src/redis-cli -h 192.168.0.100 -p 6379 shutdown
+##ssh u1 /opt/share/redis/src/redis-cli -h 192.168.0.101 -p 6380 shutdown
+##ssh u2 /opt/share/redis/src/redis-cli -h 192.168.0.102 -p 6381 shutdown
 
+redis-cli -p 6380 shutdown
+redis-cli -p 6381 shutdown
+redis-cli -p 6382 shutdown
+redis-cli -p 6383 shutdown
+redis-cli -p 6384 shutdown
+redis-cli -p 6379 shutdown
 
--- redis_start.sh
+++ redis_start.sh
@@ -5,5 +5,8 @@ pushd /opt/share/redis
 ./src/redis-server ./redis_6379.conf
 ./src/redis-server ./redis_6380.conf
 ./src/redis-server ./redis_6381.conf
+./src/redis-server ./redis_6382.conf
+./src/redis-server ./redis_6383.conf
+./src/redis-server ./redis_6384.conf
 
 popd
