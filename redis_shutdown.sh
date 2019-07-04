50a6bf94b4cc821f4a78c8401d605cdf87b57d3c
ShadowWalker <shadowwalker2718@gmail.com>
2019-07-04 05:39:08 -0400

    a

--- redis_shutdown.sh
+++ redis_shutdown.sh
@@ -4,10 +4,10 @@
 ##ssh u1 /opt/share/redis/src/redis-cli -h 192.168.0.101 -p 6380 shutdown
 ##ssh u2 /opt/share/redis/src/redis-cli -h 192.168.0.102 -p 6381 shutdown
 
-redis-cli -p 6380 shutdown
-redis-cli -p 6381 shutdown
-redis-cli -p 6382 shutdown
-redis-cli -p 6383 shutdown
-redis-cli -p 6384 shutdown
-redis-cli -p 6379 shutdown
+/opt/share/redis/src/redis-cli -p 6380 shutdown
+/opt/share/redis/src/redis-cli -p 6381 shutdown
+/opt/share/redis/src/redis-cli -p 6382 shutdown
+/opt/share/redis/src/redis-cli -p 6383 shutdown
+/opt/share/redis/src/redis-cli -p 6384 shutdown
+/opt/share/redis/src/redis-cli -p 6379 shutdown
 
